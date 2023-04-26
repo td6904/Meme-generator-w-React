4d03da56a3c1af01fd270943fb314730a2b00edf
Oignnon - <td665pro@pm.me>
2023-04-26 15:34:25 +0200

    Form working with image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,29 +1,45 @@
 import React from "react";
 import searchMeme from "../images/will.png";
-import memesData from "../memesData.js"
+import memesData from "../memesData.js";
 
 // export default function Meme() {
 //   const [memeImage, setMemeImage] = React.useState("")
 // ^^ Have to change from this to vvvv this for refactoring.
 
-  export default function Meme() {
-    const [meme, setMeme] = React.useState({
-      topText: "",
-      bottomText: "",
-      randomImage: "https://i.imgflip.com/30b1gx.jpg"
-    })
+export default function Meme() {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "https://i.imgflip.com/30b1gx.jpg",
+  });
 
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
 
   function getMemeImage() {
-    const memesArray = allMemeImages.data.memes
-    const randomNumber = Math.floor(Math.random() * memesArray.length)
-    const url = (memesArray[randomNumber].url)
-    setMeme(prevMeme => ({
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
       ...prevMeme,
-      randomImage: url
-    }))
+      randomImage: url,
+    }));
   }
+
+  function handleChange(event) {
+    const {name, value} = event.target
+    setMeme(prevMeme => {
+      return {
+        ...prevMeme,
+        [name]: value
+      }
+    })
+  }
+
+  // function handleSubmit(event) {
+  //   event.preventDefault()
+  //   console.log(meme);
+  // }
+  // Not needed I don't think
 
   return (
     <div className="form">
@@ -32,20 +48,30 @@ import memesData from "../memesData.js"
           className="text-bubble"
           type="text"
           placeholder="First piece of text..."
+          onChange={handleChange}
+          name="topText"
+          value={meme.topText}
         />
         <input
           className="text-bubble"
           type="text"
           placeholder="... Then the second piece of text"
+          onChange={handleChange}
+          name="bottomText"
+          value={meme.bottomText}
         />
       </div>
       <div className="submit">
-      <button type="submit" onClick={getMemeImage}>
-        Get a new meme image
-        <img src={searchMeme} alt="will" width="20px" />
-      </button>
+        <button type="submit" onClick={getMemeImage}>
+          Get a new meme image
+          <img src={searchMeme} alt="will" width="20px" />
+        </button>
       </div>
-      <img src={meme.randomImage} alt="meme" />
+      <div className="meme">
+      <img src={meme.randomImage} alt="meme" className="main-image" />
+      <h2 className="meme-text-top">{meme.topText}</h2>
+      <h2 className="meme-text-bottom">{meme.bottomText}</h2>
+      </div>
     </div>
   );
 }
